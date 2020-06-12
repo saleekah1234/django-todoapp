b7c2d13f5a2bac14dd5d3513019cc7f9d9004938
saleekah1234 <60786732+saleekah1234@users.noreply.github.com>
2020-06-12 10:17:30 +0530

    Add files via upload

--- todo/urls.py
+++ todo/urls.py
@@ -2,6 +2,8 @@ from django.urls import include,path
 from . import views
 from django.contrib import admin
 
-urlpatterns=[path('',views.todo_view,name='todo_view'),
+urlpatterns=[
+    path('',views.todo_view,name='todo_view'),
     path('addTodo/',views.add_todo,name='add_todo'),
-    path('deleteTodo/<int:todo_id>/',views.delete_todo,name=' delete_todo')]
+    path('deleteTodo/<int:pk>/',views.delete_todo,name=' delete_todo')
+    ]
--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,5 @@
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect
 from .models import TodoItem
 
@@ -11,10 +11,10 @@ def todo_view(request):
 def add_todo(request):
     new_item = TodoItem(content = request.POST['content'])
     new_item.save()
-    return HttpResponseRedirect('/todo/')
+    return redirect('/')
 
-def delete_todo(request, todo_id):
-    item_to_delete = TodoItem.objects.get(id=todo_id)
+def delete_todo(request, pk):
+    item_to_delete = TodoItem.objects.get(id=pk)
     item_to_delete.delete()
-    return HttpResponseRedirect('/todo/')
+    return redirect('/')
 
